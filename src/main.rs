f446594a4a9e56b6a7d6e1b2f87d66928baff311
akosawointa@gmail.com <akosawointa@gmail.com>
2025-01-07 03:54:48 +0100

    call read_version, initialize call to a variable and print the value

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-#[allow(unused)]
+#[allow(unused_variables)]
+fn read_version(transaction_hex: &str) -> u32{
+    return 1
+}
 
+#[allow(unused_variables)]
 fn main (){
+   let _version: u32 = read_version("1075db55d416d3ca199f55b6084e2115b9345e16c5cf302fc80e9d5fbf5d48d");
+   
 }
 
